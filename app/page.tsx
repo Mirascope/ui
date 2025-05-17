35bbbd1aa0568b8363c96a61eda1c10e9ee9e0f5
Brendan <brendan.kao@sotai.ai>
2025-05-17 12:30:29 -0400

    feat: update mirascope ui to use shadcn registry

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+// This page displays items from the custom registry.
+// You are free to implement this with your own design as needed.
+
+export default function Home() {
+  return (
+    <div className='max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8'>
+      <h1>Mirascope UI</h1>
+    </div>
+  );
+}
